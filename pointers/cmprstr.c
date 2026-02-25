c780fb85a305ef3258813cfe858bfa196deb9031
Logesh <logeshplogesh7@gmail.com>
2026-02-25 23:05:26 +0530

    Update cmprstr.c

--- pointers/cmprstr.c
+++ pointers/cmprstr.c
@@ -1,20 +1,32 @@
 #include<stdio.h>
 int main()
 {
-	char str [16]={"ORGINAL GANGSTER"};
+	char str [16]={"ORGIN GANGSTER"};
 	char *p=str;
 
 	char str1 [16]={"ORGINAL GANGSTER"}; 
 char *pp=str1;
 	int i=0;	
-
-if(*(p)==*(pp))
-{
-		printf("equal");}
+	int s;
+while(*(p+i)!='\0'){
+	
+if (*(p+i)==*(pp+i))
+{ s=1;
+	}
 
 else{
-printf("str1 and str2 is  not equal ");
+s=0;
+    break;
 }
+
+	i++;
+}
+if (s)
+{printf("str is equal ");}
+
+else
+
+    printf("str is not equal " );
 
 return 0;
 }
